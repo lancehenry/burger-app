7f6a139cfa236a648d8b642d0fa3788c9d636861
lschmittling <lanceschmittling@Lances-MBP>
2018-04-24 21:28:27 -0500

    Getting more files set up

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,3 +3,10 @@
 
 // Create the router for this app (probably learn about this in the Monday/Wednesday class)
 // Export the router
+
+var express = require('express');
+var burger = require('../models/burger.js');
+
+router.get('/', function(req, res) {
+    res.redirect('/burger')
+})
--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 // Dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var mysql = require('mysql');
+
+var app = express();
