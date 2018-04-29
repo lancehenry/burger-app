f392e62219433694ba2e66f76099272540651538
lschmittling <lanceschmittling@Lances-MBP>
2018-04-29 10:28:28 -0500

    Made some changes to burgers_controllers.js file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,30 +16,26 @@ router.get('/', function(req, res) {
   });
 });
 
-router.post('/', function(req, res) {
+router.post('/burgers', function(req, res) {
   burger.create(
-    ['burger_name', 'devoured'],
-    [req.body.burger_name, req.body.devoured],
-    function() {
+    ['burger_name'],
+    [req.body.burger_name],
+    function(data) {
       res.redirect('/');
-    }
-  );
+    });
 });
 
-router.put('/:id', function(req, res) {
+router.put('/burgers/:id', function(req, res) {
   var condition = 'id = ' + req.params.id;
-
-  console.log('condition', condition);
 
   burger.update(
     {
-      devoured: req.body.devoured
+      devoured: true
     },
     condition,
     function() {
       res.redirect('/');
-    }
-  );
+    });
 });
 
 // Export routes for server.js to use
