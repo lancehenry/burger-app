46eb742f78be0528778bedf8d3dfff1ce4bde0b2
lschmittling <lanceschmittling@Lances-MBP>
2018-04-29 10:31:18 -0500

    connection.js update

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
+    port: 3306,
     host: 'localhost',
     user: 'root',
     password: '',
